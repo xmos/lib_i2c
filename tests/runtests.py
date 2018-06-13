d8b7061c766d6b18bf4f9ac9f344df0a29b1d24f
Peter Hedinger <peter@xmos.com>
2018-06-13 13:08:19 +0100

    Fix copyright dates and fixed location of shebang directives.

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2014-2018, XMOS Ltd, All rights reserved
 #!/usr/bin/env python
+# Copyright (c) 2014-2018, XMOS Ltd, All rights reserved
 import xmostest
 from i2c_master_checker import I2CMasterChecker
 
