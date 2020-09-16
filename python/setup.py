af8eaee31bcba59e64d1f32056e22fe61bf6bf03
lucianom <lucianom@xmos.com>
2020-09-16 17:34:36 +0100

    Update lib name

--- python/setup.py
+++ python/setup.py
@@ -9,7 +9,7 @@ import setuptools
 # least the same minor version as specified in the requirements.txt file.
 # The same modules should appear in the requirements.txt file as given below.
 setuptools.setup(
-    name='lib_i2s',
+    name='lib_i2c',
     packages=setuptools.find_packages(),
     install_requires=[
         'flake8~=3.8',
