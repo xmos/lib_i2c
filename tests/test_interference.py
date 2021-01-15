b95067559362fd056a75627328492dafe296f2fa
lucianom <lucianom@xmos.com>
2021-01-15 09:28:40 +0000

    Update copyright dates and fix error

--- tests/test_interference.py
+++ tests/test_interference.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2018, XMOS Ltd, All rights reserved
+# Copyright (c) 2014-2021, XMOS Ltd, All rights reserved
 import xmostest
 from i2c_master_checker import I2CMasterChecker
 import os
@@ -38,6 +38,6 @@ def do_master_test(arch, stop):
                               tester = tester)
 
 def runtest():
-  for arch in ['xs1', 'xs2', , 'xcoreai']:
+  for arch in ['xs1', 'xs2', 'xcoreai']:
     for stop in ['stop', 'no_stop']:
       do_master_test(arch, stop)
--- tests/test_master_acks.py
+++ tests/test_master_acks.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2018, XMOS Ltd, All rights reserved
+# Copyright (c) 2014-2021, XMOS Ltd, All rights reserved
 import xmostest
 from i2c_master_checker import I2CMasterChecker
 
