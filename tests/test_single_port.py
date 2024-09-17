a98d418490b0f4f8db2fc904b220bd3131c2491b
Shuchita Khare <shuchitak@xmos.com>
2024-09-17 10:43:20 +0100

    Changed test_repeated_start and test_single_port to run with pytest

--- tests/test_single_port.py
+++ tests/test_single_port.py
@@ -1,44 +1,48 @@
-# Copyright 2014-2021 XMOS LIMITED.
+# Copyright 2014-2024 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
-import xmostest
+from pathlib import Path
+import Pyxsim
+import pytest
+import json
 from i2c_master_checker import I2CMasterChecker
-import os
 
-def do_sp_test(stop, speed):
-    resources = xmostest.request_resource("xsim")
 
-    binary = 'i2c_sp_test/bin/%(speed)d_%(stop)s_xs2/i2c_sp_test_%(speed)d_%(stop)s_xs2.xe' % {
-      'speed' : speed,
-      'stop'  : stop
-    }
+test_name = "i2c_sp_test"
+with open(Path(__file__).parent / f"{test_name}/test_params.json") as f:
+    params = json.load(f)
+
+@pytest.mark.parametrize("speed", params['SPEEDS'])
+@pytest.mark.parametrize("stop", params['STOPS'])
+def test_single_port(capfd, request, nightly, speed, stop):
+
+    cwd = Path(request.fspath).parent
+    arch = "xcoreai"
+    cfg = f"{speed}_{stop}_{arch}"
+    binary = f'{cwd}/{test_name}/bin/{cfg}/{test_name}_{cfg}.xe'
+
+    assert Path(binary).exists(), f"Cannot find {binary}"
+
 
     checker = I2CMasterChecker("tile[0]:XS1_PORT_8A.1",
-                               "tile[0]:XS1_PORT_8A.3",
-                               tx_data=[0x99, 0x3a, 0xff, 0xaa, 0xbb],
-                               expected_speed=speed,
-                               # Test some sequences ending with ACK, some with NACK
-                               ack_sequence=[True, True, False,
-                                             True, True, True, False])
+                            "tile[0]:XS1_PORT_8A.3",
+                            tx_data=[0x99, 0x3a, 0xff, 0xaa, 0xbb],
+                            expected_speed=speed,
+                            # Test some sequences ending with ACK, some with NACK
+                            ack_sequence=[True, True, False,
+                                            True, True, True, False])
 
-    tester = xmostest.ComparisonTester(open('single_port_test_%s.expect' % stop),
-                                     'lib_i2c', 'i2c_master_sim_tests',
-                                     'single_port_test',
-                                     {'speed':speed, 'stop':stop},
-                                     regexp=True)
+    tester = Pyxsim.testers.AssertiveComparisonTester(
+        f'{cwd}/single_port_test_{stop}.expect',
+        regexp = True,
+        ordered = True,
+        suppress_multidrive_messages=True,
+    )
 
-    if speed == 10:
-        tester.set_min_testlevel('nightly')
-
-    sim_args = ['--weak-external-drive']
-
-    xmostest.run_on_simulator(resources['xsim'], binary,
-                              simthreads = [checker],
-                              simargs=sim_args,
-                              suppress_multidrive_messages = True,
-                              tester = tester)
-
-
-def runtest():
-    for stop in ['stop', 'no_stop']:
-      for speed in [400, 100, 10]:
-          do_sp_test(stop, speed)
+    Pyxsim.run_on_simulator_(
+        binary,
+        tester = tester,
+        do_xe_prebuild = False,
+        simthreads = [checker],
+        simargs=['--weak-external-drive'],
+        capfd=capfd
+        )
