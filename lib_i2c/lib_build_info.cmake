e9d565e2de43cc6e4a9c6a611b7bfb76760cf7c1
Ross Owen <ross@xmos.com>
2026-07-31 18:35:04 +0100

    Os

--- lib_i2c/lib_build_info.cmake
+++ lib_i2c/lib_build_info.cmake
@@ -6,7 +6,7 @@ set(LIB_INCLUDES api)
 
 set(LIB_DEPENDENT_MODULES "lib_xassert(4.3.2)")
 
-set(LIB_COMPILER_FLAGS  -O3
+set(LIB_COMPILER_FLAGS  -Os
                         -Wall
                         -Wextra
                         -Wsign-compare
